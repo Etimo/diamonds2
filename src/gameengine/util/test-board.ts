c67a982f3b9637c383b55a95b9629ea8f027aec1
Daniel Winther <daniel.winther@quinyx.com>
2019-09-26 13:17:47 +0200

    Add test board

---
+++ src/gameengine/util/test-board.ts
@@ -0,0 +1,12 @@
+import { Board } from '../board';
+
+export default function createTestBoard(): Board {
+    return new Board({
+        height: 10,
+        width: 10,
+        minimumDelayBetweenMoves: 100,
+      },
+      [],
+      console
+      );
+}
