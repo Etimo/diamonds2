16e9ad35239caf70d8330e5c72efdd6b79b78bbd
Daniel Winther <daniel.winther@quinyx.com>
2019-09-27 13:23:40 +0200

    Add test for diamond game object

---
+++ src/gameengine/gameobjects/diamond/diamond.spec.ts
@@ -0,0 +1,19 @@
+import { BotGameObject } from "../bot/bot";
+import { DiamondGameObject } from "./diamond";
+import createTestBoard from "../../util/test-board";
+
+let gameObject: DiamondGameObject;
+
+beforeAll(() => {
+  gameObject = new DiamondGameObject({x:0, y:0}, 1);
+});
+
+test("Stepping on a diamond increases score for bot", () => {
+  const bot = new BotGameObject({ x: 0, y: 0 });
+  bot.diamonds = 0;
+  bot.inventorySize = 5;
+  
+  gameObject.onGameObjectEntered(bot, createTestBoard());
+  
+  expect(bot.diamonds).toBe(1);
+});
