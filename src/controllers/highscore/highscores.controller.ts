3061b1b836505c43c27f0f921d1c9dfdfbc3bac7
JassyrBravo <31710951+JassyrBravo@users.noreply.github.com>
2019-11-21 13:15:14 +0100

    highscores service and endpoints #11 #12

--- src/controllers/highscore/highscores.controller.ts
+++ src/controllers/highscore/highscores.controller.ts
@@ -1,17 +1,26 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Body, Post } from "@nestjs/common";
 import { ApiUseTags, ApiResponse } from "@nestjs/swagger";
 import { HighscoreDto } from "src/models/highscore.dto";
+import { HighScoresService } from "src/services/high-scores.service";
 @ApiUseTags("Highscores")
-@Controller("api/Highscores")
+@Controller("api/highscores")
 export class HighscoresController {
+  constructor(private highScoresService: HighScoresService) {}
   @ApiResponse({
     status: 200,
     description: "Returns highscores",
     type: HighscoreDto,
     isArray: true,
   })
+  @Post()
+  async create(
+    @Body() highScoresRegistration: HighscoreDto,
+  ): Promise<HighscoreDto[]> {
+    await this.highScoresService.add(highScoresRegistration);
+    return this.highScoresService.all();
+  }
   @Get()
-  async get(): Promise<HighscoreDto[]> {
-    return;
+  async listAll() {
+    return this.highScoresService.all();
   }
 }
