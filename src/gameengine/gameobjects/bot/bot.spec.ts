5af90d4f44eeefe7b165c814330e07b42246a5f5
Daniel Winther <daniel.winther@quinyx.com>
2019-11-26 16:06:06 +0100

    Add tests for bot feature

---
+++ src/gameengine/gameobjects/bot/bot.spec.ts
@@ -0,0 +1,13 @@
+import { BotGameObject } from "../bot/bot";
+
+let bot: BotGameObject;
+
+beforeAll(() => {
+  bot = new BotGameObject({ x: 0, y: 0 });
+});
+
+test("Has properties", () => {
+  const bot = new BotGameObject({ x: 0, y: 0 });
+  bot.expiresAt = new Date();
+  expect(bot.properties).not.toBe(null);
+});
