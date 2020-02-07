1d1a306e6350ed894459dde23d57535a7ee82fa9
Jens Hunt <himself@jenshunt.se>
2020-02-07 19:38:10 +0100

    Dont join twice (#62)
    * Dont join twice
    
    * Remove duplicate IBot import
    
    Co-authored-by: Daniel Winther <daniel.kirgiopoulos@gmail.com>

---
+++ backend/src/services/board.service.spec.ts
@@ -0,0 +1,41 @@
+import { BotsService } from "./bots.service";
+import { IdService } from "./id.service";
+import { BoardsService } from "./board.service";
+import { HighScoresService } from "./high-scores.service";
+import { CustomLogger } from "../logger";
+import UnauthorizedError from "../errors/unauthorized.error";
+import { IBot } from "../interfaces/bot.interface";
+import NotFoundError from "../errors/not-found.error";
+import ConflictError from "../errors/conflict.error";
+
+let boardsService: BoardsService;
+let botService: BotsService;
+let highScoreService: HighScoresService;
+const dummyBoardId = 1111111;
+const dummyBoardToken = "dummy";
+const dummyBotId = "dummyId";
+
+beforeEach(() => {
+  const idService = new IdService();
+  botService = new BotsService(idService);
+  highScoreService = new HighScoresService(idService);
+  boardsService = new BoardsService(
+    botService,
+    highScoreService,
+    new CustomLogger(),
+  );
+});
+
+test("Should throw UnauthorizedError when bot not exists", async () => {
+  spyOn(botService, "get").and.returnValue(undefined);
+  await expect(
+    boardsService.join(dummyBoardId, dummyBoardToken),
+  ).rejects.toThrowError(UnauthorizedError);
+});
+
+test("Should throw NotFoundError when board not exists", async () => {
+  spyOn(botService, "get").and.returnValue({} as IBot);
+  await expect(
+    boardsService.join(dummyBoardId, dummyBoardToken),
+  ).rejects.toThrowError(NotFoundError);
+});
