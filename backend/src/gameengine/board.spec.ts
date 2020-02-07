e32b5a28cc0b2e6af73e0b11f8cf9ac88137116c
Philip Forsberg <philip.forsberg@etimo.se>
2020-02-07 16:49:19 +0100

    Resolved conflicts with develop and fixed test.

--- backend/src/gameengine/board.spec.ts
+++ backend/src/gameengine/board.spec.ts
@@ -1,20 +1,71 @@
 import { Board } from "./board";
 import createTestBoard from "./util/test-board";
 import { BotGameObject } from "./gameobjects/bot/bot";
-import { DiamondButtonGameObject } from "./gameobjects/diamond-button/diamond-button";
-import { DiamondGameObject } from "./gameobjects/diamond/diamond";
+import { AbstractGameObject } from "./gameobjects/abstract-game-object";
+import { BaseGameObject } from "./gameobjects/base/base";
+import { DiamondProvider } from "./gameobjects/diamond/diamond-provider";
+import { BotProvider } from "./gameobjects/bot/bot-provider";
+import { create } from "domain";
+import { IBot } from "src/interfaces/bot.interface";
 
 let board: Board;
 let opponent: BotGameObject;
-let button: DiamondButtonGameObject;
-let diamond: DiamondGameObject;
+let provider: DiamondProvider;
+const botExampleData: IBot = {
+  id: "1",
+  email: "email",
+  name: "name",
+  token: "1",
+};
 
 beforeEach(() => {
+  provider = new DiamondProvider({
+    generationRatio: 0.1,
+    minRatioForGeneration: 0.01,
+  });
   board = createTestBoard();
   opponent = new BotGameObject({ x: 1, y: 0 });
   board.addGameObjects([opponent]);
-  button = new DiamondButtonGameObject({ x: 0, y: 0 });
-  diamond = new DiamondGameObject({ x: 0, y: 0 }, 1);
+  jest.useFakeTimers();
+});
+
+test("board has some kind of id", () => {
+  expect(board.getId()).toBeDefined();
+});
+
+describe("sessionFinishedCallbacks and join", () => {
+  let provider: BotProvider;
+  beforeEach(() => {
+    provider = new BotProvider({
+      inventorySize: 5,
+    });
+    board = createTestBoard([provider]);
+  });
+  test("join creates expiration timer and callbacks are invoked when sessions are finished", async () => {
+    let result = false;
+    board.registerSessionFinishedCallback(() => {
+      result = true;
+    });
+    await board.join(botExampleData);
+
+    jest.runAllTimers();
+
+    expect(result).toBeTruthy();
+  });
+
+  test("join notifies providers", async () => {
+    spyOn(provider, "onBotJoined");
+
+    await board.join(botExampleData);
+
+    expect(provider.onBotJoined).toHaveBeenCalled();
+  });
+
+  test("possible to get bot when joined", async () => {
+    await board.join(botExampleData);
+
+    expect(board.getBot(botExampleData.token)).toEqual(botExampleData);
+  });
 });
 
 describe("trySetGameObjectPosition", () => {
@@ -65,7 +116,7 @@ describe("trySetGameObjectPosition", () => {
     expect(bot.position).toStrictEqual({ x: 1, y: 0 });
   });
 
-  test("when gameobject not allow move bot stays", () => {
+  test("when gameobject not allowed to move bot stays", () => {
     const initialPosition = { x: 0, y: 0 };
     const bot = new BotGameObject(initialPosition);
     spyOn(opponent, "canGameObjectEnter").and.returnValue(false);
@@ -88,18 +139,197 @@ describe("trySetGameObjectPosition", () => {
     expect(bot.position).toStrictEqual({ x: 1, y: 0 });
     expect(opponent.onGameObjectEntered).toHaveBeenCalledTimes(1);
   });
+});
 
-  test("Removes buttons from board", () => {
-    board.addGameObjects([button]);
-    board.removeGameObjectsByType(DiamondButtonGameObject);
-
-    expect(board.getGameObjectsByType(DiamondButtonGameObject).length).toBe(0);
+describe("isCellEmpty", () => {
+  test("returns false if at least one game object is located", () => {
+    expect(
+      board.isCellEmpty(opponent.position.x, opponent.position.y),
+    ).toBeFalsy();
   });
 
-  test("Removes diamonds from board", () => {
-    board.addGameObjects([diamond]);
-    board.removeGameObjectsByType(DiamondGameObject);
+  test("returns true if no game objects", () => {
+    expect(
+      board.isCellEmpty(opponent.position.x + 1, opponent.position.y),
+    ).toBeTruthy();
+  });
+});
 
-    expect(board.getGameObjectsByType(DiamondGameObject).length).toBe(0);
+describe("getEmptyPosition", () => {
+  test("returns an empty position", () => {
+    const empty = board.getEmptyPosition();
+    expect(empty).not.toEqual(opponent.position);
+  });
+
+  test("returns an empty position even though random checks fail", () => {
+    spyOn(board, "getRandomPosition").and.returnValue(opponent.position);
+    const empty = board.getEmptyPosition();
+    expect(empty).not.toEqual(opponent.position);
+  });
+
+  test("returns null if no empty positions are found", () => {
+    spyOn(board, "getRandomPosition").and.returnValue(opponent.position);
+    spyOn(board, "isCellEmpty").and.returnValue(false);
+    const empty = board.getEmptyPosition();
+    expect(empty).toBeNull();
+  });
+});
+
+test("getAllGameObjects returns a list of existing game objects", () => {
+  expect(board.getAllGameObjects().length).toBe(1);
+});
+
+describe("getGameObjectsOnPosition", () => {
+  test("returns all game objects on position", () => {
+    expect(board.getGameObjectsOnPosition(opponent.position)).toEqual([
+      opponent,
+    ]);
+  });
+  test("returns empty list if no game objects on position", () => {
+    expect(board.getGameObjectsOnPosition({ x: 0, y: 0 })).toEqual([]);
+  });
+  test("returns multiple game objects on same position", () => {
+    const dummyObject: BotGameObject = new BotGameObject(opponent.position);
+    board.addGameObjects([dummyObject]);
+
+    expect(board.getGameObjectsOnPosition(opponent.position)).toEqual([
+      opponent,
+      dummyObject,
+    ]);
+  });
+});
+
+describe("canGameObjectEnter", () => {
+  let blockingGameObject: AbstractGameObject;
+  let allowingGameObject: AbstractGameObject;
+
+  beforeEach(() => {
+    blockingGameObject = new BotGameObject({
+      x: opponent.x + 1,
+      y: opponent.y,
+    });
+    spyOn(blockingGameObject, "canGameObjectEnter").and.returnValue(false);
+
+    allowingGameObject = new BotGameObject({
+      x: opponent.x,
+      y: opponent.y + 1,
+    });
+    spyOn(allowingGameObject, "canGameObjectEnter").and.returnValue(true);
+
+    board.addGameObjects([blockingGameObject, allowingGameObject]);
+  });
+
+  test("returns false if trying to move to a blocking game object", () => {
+    expect(
+      board.canGameObjectEnter(opponent, blockingGameObject.position),
+    ).toBeFalsy();
+  });
+
+  test("returns true if trying to move to an allowing game object", () => {
+    expect(
+      board.canGameObjectEnter(opponent, allowingGameObject.position),
+    ).toBeTruthy();
+  });
+});
+
+describe("canGameObjectLeave", () => {
+  let blockingGameObject: AbstractGameObject;
+  let allowingGameObject: AbstractGameObject;
+
+  beforeEach(() => {
+    blockingGameObject = new BotGameObject({
+      x: opponent.x + 1,
+      y: opponent.y,
+    });
+    spyOn(blockingGameObject, "canGameObjectLeave").and.returnValue(false);
+
+    allowingGameObject = new BotGameObject({
+      x: opponent.x,
+      y: opponent.y + 1,
+    });
+    spyOn(allowingGameObject, "canGameObjectLeave").and.returnValue(true);
+
+    board.addGameObjects([blockingGameObject, allowingGameObject]);
+  });
+
+  test("returns false if trying to move to a blocking game object", () => {
+    expect(
+      board.canGameObjectLeave(opponent, blockingGameObject.position),
+    ).toBeFalsy();
+  });
+
+  test("returns true if trying to move to an allowing game object", () => {
+    expect(
+      board.canGameObjectLeave(opponent, allowingGameObject.position),
+    ).toBeTruthy();
+  });
+});
+
+describe("removeGameObject", () => {
+  test("removes game object from list", () => {
+    board.removeGameObject(opponent);
+
+    expect(board.getAllGameObjects().length).toBe(0);
+  });
+  test("calls onGameObjectRemoved on game object", () => {
+    spyOn(opponent, "onGameObjectRemoved");
+
+    board.removeGameObject(opponent);
+
+    expect(opponent.onGameObjectRemoved).toHaveBeenCalled();
+  });
+});
+
+describe("removeGameObjectsByType", () => {
+  let baseGameObject: BaseGameObject;
+  beforeEach(() => {
+    baseGameObject = new BaseGameObject({ x: 0, y: 0 });
+    board.addGameObjects([baseGameObject]);
+  });
+
+  test("removes from array", () => {
+    board.removeGameObjectsByType(BaseGameObject);
+
+    expect(board.getAllGameObjects()).toEqual([opponent]);
+  });
+
+  test("calls onGameObjectRemoved on each removed game object", () => {
+    spyOn(opponent, "onGameObjectRemoved");
+
+    board.removeGameObjectsByType(BotGameObject);
+
+    expect(opponent.onGameObjectRemoved).toHaveBeenCalled();
+  });
+  test("notifies providers", () => {
+    board = createTestBoard([provider]);
+    spyOn(provider, "onGameObjectsRemoved");
+
+    board.removeGameObjectsByType(BotGameObject);
+
+    expect(provider.onGameObjectsRemoved).toHaveBeenCalled();
+  });
+});
+
+describe("addGameObjects", () => {
+  let baseGameObject: BaseGameObject;
+  beforeEach(() => {
+    board = createTestBoard([provider]);
+    baseGameObject = new BaseGameObject({ x: 0, y: 0 });
+  });
+
+  test("adds to array", () => {
+    let countBefore = board.getAllGameObjects().length;
+
+    board.addGameObjects([baseGameObject]);
+
+    expect(board.getAllGameObjects().length).toBe(countBefore + 1);
+  });
+
+  test("notifies providers", () => {
+    spyOn(provider, "onGameObjectsAdded");
+
+    board.addGameObjects([baseGameObject]);
+
+    expect(provider.onGameObjectsAdded).toHaveBeenCalled();
   });
 });
--- backend/src/services/board.service.ts
+++ backend/src/services/board.service.ts
@@ -1,26 +1,28 @@
-import { Injectable, Scope, NotFoundException } from "@nestjs/common";
+import { Injectable, Scope } from "@nestjs/common";
+import { IPosition } from "src/common/interfaces/position.interface";
+import { MoveDirection } from "src/enums/move-direction.enum";
+import ConflictError from "src/errors/conflict.error";
+import ForbiddenError from "src/errors/forbidden.error";
+import NotFoundError from "src/errors/not-found.error";
+import UnauthorizedError from "src/errors/unauthorized.error";
 import { Board } from "src/gameengine/board";
-import { CustomLogger } from "src/logger";
-import { DiamondButtonProvider } from "src/gameengine/gameobjects/diamond-button/diamond-button-provider";
-import { BaseProvider } from "src/gameengine/gameobjects/base/base-provider";
-import { DiamondProvider } from "src/gameengine/gameobjects/diamond/diamond-provider";
-import { BotProvider } from "src/gameengine/gameobjects/bot/bot-provider";
 import { BoardConfig } from "src/gameengine/board-config";
+import { BaseProvider } from "src/gameengine/gameobjects/base/base-provider";
+import { BotProvider } from "src/gameengine/gameobjects/bot/bot-provider";
+import { DiamondButtonProvider } from "src/gameengine/gameobjects/diamond-button/diamond-button-provider";
+import { DiamondProvider } from "src/gameengine/gameobjects/diamond/diamond-provider";
+import { OperationQueueBoard } from "src/gameengine/operation-queue-board";
+import { CustomLogger } from "src/logger";
 import { BoardDto } from "src/models/board.dto";
 import { GameObjectDto } from "src/models/game-object.dto";
-import NotFoundError from "src/errors/not-found.error";
 import { BotsService } from "./bots.service";
-import UnauthorizedError from "src/errors/unauthorized.error";
-import { MoveDirection } from "src/enums/move-direction.enum";
-import { IPosition } from "src/common/interfaces/position.interface";
-import { OperationQueueBoard } from "src/gameengine/operation-queue-board";
 import { HighScoresService } from "./high-scores.service";
-import ForbiddenError from "src/errors/forbidden.error";
-import ConflictError from "src/errors/conflict.error";
+import { IBot } from "src/interfaces/bot.interface";
 
 @Injectable({ scope: Scope.DEFAULT })
 export class BoardsService {
   private boards: OperationQueueBoard[] = [];
+  private lastMoveTimes: {};
 
   constructor(
     private botsService: BotsService,
@@ -51,7 +53,7 @@ export class BoardsService {
    * Return a specific board.
    * @param id The id of the board to return.
    */
-  public getById(id: string): BoardDto {
+  public getById(id: number): BoardDto {
     const board = this.getBoardById(id);
     if (board) {
       return this.getAsDto(board);
@@ -64,7 +66,7 @@ export class BoardsService {
    * @param boardId
    * @param bot
    */
-  public async join(boardId: string, botToken: string) {
+  public async join(boardId: number, botToken: string) {
     const bot = await this.botsService.get(botToken);
     if (!bot) {
       throw new UnauthorizedError("Invalid botToken");
@@ -82,7 +84,7 @@ export class BoardsService {
   }
 
   public async move(
-    boardId: string,
+    boardId: number,
     botToken: string,
     direction: MoveDirection,
   ) {
@@ -98,6 +100,13 @@ export class BoardsService {
       throw new UnauthorizedError("Invalid botToken");
     }
 
+    // Rate limit moves
+    if (this.moveIsRateLimited(board, bot)) {
+      const delay = board.getConfig().minimumDelayBetweenMoves;
+      throw new ConflictError(`Minimum delay between moves: (${delay} ms`);
+    }
+    board.updateLastMove(bot);
+
     const result = await board.enqueueMove(
       bot,
       this.directionToDelta(direction),
@@ -110,7 +119,14 @@ export class BoardsService {
     return this.getAsDto(board);
   }
 
-  private getBoardById(id: string): OperationQueueBoard {
+  private moveIsRateLimited(board: OperationQueueBoard, bot: IBot) {
+    const lastMove = board.getLastMove(bot);
+    const timeBetweenMoves = board.getConfig().minimumDelayBetweenMoves;
+    const now = Date.now();
+    return lastMove > now - timeBetweenMoves;
+  }
+
+  private getBoardById(id: number): OperationQueueBoard {
     return this.boards.find(b => b.getId() === id);
   }
 
@@ -139,7 +155,7 @@ export class BoardsService {
    */
   private getAsDto(board: Board): BoardDto {
     return {
-      id: `${board.getId()}`,
+      id: board.getId(),
       width: board.width,
       height: board.height,
       minimumDelayBetweenMoves: board.getConfig().minimumDelayBetweenMoves,
