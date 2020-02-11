e3c1dfb8ce714ad238ae4e3bd62d8c42368de9eb
Daniel Winther <daniel.kirgiopoulos@gmail.com>
2020-02-11 21:13:29 +0100

    Try building using compose and pushing to docker hub from travis

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Get git hash
+HASH=$(git rev-parse --short HEAD)
+
+# docker-compose -f docker-compose.prod.yml build --parallel
+
+echo "Tagging images..."
+docker tag diamonds2_frontend:latest etimodanielwinther/diamonds2_frontend:latest
+docker tag diamonds2_frontend:latest etimodanielwinther/diamonds2_frontend:git-$HASH
+docker tag diamonds2_backend:latest etimodanielwinther/diamonds2_backend:latest
+docker tag diamonds2_backend:latest etimodanielwinther/diamonds2_backend:git-$HASH
+
+echo "Pushing images..."
+# docker push etimodanielwinther/diamonds2_frontend:latest
+# docker push etimodanielwinther/diamonds2_backend:latest
