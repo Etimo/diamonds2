c512c6a31fc0efa83e7354a14367c178c7833809
Daniel Winther <daniel.winther@quinyx.com>
2020-02-14 15:03:12 +0100

    Enable pulling of docker images

--- deployer/redeploy.sh
+++ deployer/redeploy.sh
@@ -10,8 +10,8 @@ fi
 echo "Deploying app for hash $COMMIT_HASH..."
 
 # Pull new images from docker hub
-# docker pull etimodanielwinther/diamonds2_frontend:git-$COMMIT_HASH
-# docker pull etimodanielwinther/diamonds2_backend:git-$COMMIT_HASH
+docker pull etimodanielwinther/diamonds2_frontend:git-$COMMIT_HASH
+docker pull etimodanielwinther/diamonds2_backend:git-$COMMIT_HASH
 
 # Redeploy
 echo TODO
