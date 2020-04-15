48cdbe9f5a91c147e37aaf1aab2bec135a0077c1
Lukas Lindqvist <ljukas@users.noreply.github.com>
2020-04-15 12:49:11 +0200

    Added posibility to choose which board to show on the frontend. (#87)

--- frontend/src/hooks/useBoard.js
+++ frontend/src/hooks/useBoard.js
@@ -11,31 +11,33 @@ const createBoard = ({ width, height, gameObjects }) => {
     for (let x = 0; x < width; x++) {
       rows[y][x] = {
         type: "",
-        properties: {}
+        properties: {},
       };
     }
   }
 
   // Add game objects
-  gameObjects.forEach(go => {
+  gameObjects.forEach((go) => {
     rows[go.position.y][go.position.x] = {
       ...rows[go.position.y][go.position.x],
       type: rows[go.position.y][go.position.x].type + go.type,
-      properties: go.properties
+      properties: go.properties,
     };
   });
   return rows;
 };
 
-export default (url, delay) => {
+export default (boardId, delay) => {
   const [rows, setRows] = useState([[]]);
   const [bots, setBots] = useState([]);
-  const boardData = useFetchRepeatedly(url, delay, {});
+  const boardData = useFetchRepeatedly(`/api/boards/${boardId}`, delay, {});
 
   useEffect(() => {
     if (!_.isEmpty(boardData)) {
       setRows(createBoard(boardData));
-      setBots(boardData.gameObjects.filter(b => b.type === botGameObjectName));
+      setBots(
+        boardData.gameObjects.filter((b) => b.type === botGameObjectName)
+      );
     }
   }, [boardData]);
 
