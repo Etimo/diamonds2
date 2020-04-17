1f99e7e319749914eaf95fc45c8d579a1ce9dab1
Daniel Winther <daniel.kirgiopoulos@gmail.com>
2020-04-17 08:47:17 +0200

    Generate real uuids in prod for idservice

--- backend/src/services/id.service.ts
+++ backend/src/services/id.service.ts
@@ -1,11 +1,17 @@
 import { Injectable } from "@nestjs/common";
+import { v4 as uuidv4 } from "uuid";
 
 @Injectable()
 export class IdService {
   private counter = 1;
 
   next(): string {
-    return `${this.counter++}`;
+    const env = process.env["ENVIRONMENT"];
+    if (env == "development") {
+      return `${this.counter++}`;
+    } else {
+      return uuidv4();
+    }
     // return `${this.counter++}-${Math.random()}`;
   }
 }
