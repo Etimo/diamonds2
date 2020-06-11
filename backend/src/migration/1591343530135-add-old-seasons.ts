1b9954cd564aa49a74a4d0a386f3b2024820d379
Philip Forsberg <philip.forsberg@etimo.se>
2020-06-11 12:34:50 +0200

    Added data from old seasons and migrations.

---
+++ backend/src/migration/1591343530135-add-old-seasons.ts
@@ -0,0 +1,51 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class addOldSeasons1591343530135 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.manager
+      .createQueryBuilder()
+      .insert()
+      .into("seasons")
+      .values({
+        name: "Linköping VT 2020",
+        startDate: new Date(2020, 2, 1),
+        endDate: new Date(2020, 4, 20),
+      })
+      .execute();
+
+    await queryRunner.manager
+      .createQueryBuilder()
+      .insert()
+      .into("seasons")
+      .values({
+        name: "Linköping VT 2019",
+        startDate: new Date(2019, 2, 1),
+        endDate: new Date(2019, 4, 20),
+      })
+      .execute();
+
+    await queryRunner.manager
+      .createQueryBuilder()
+      .insert()
+      .into("seasons")
+      .values({
+        name: "Linköping VT 2018",
+        startDate: new Date(2018, 2, 1),
+        endDate: new Date(2018, 4, 20),
+      })
+      .execute();
+
+    await queryRunner.manager
+      .createQueryBuilder()
+      .insert()
+      .into("seasons")
+      .values({
+        name: "Linköping VT 2017",
+        startDate: new Date(2017, 2, 1),
+        endDate: new Date(2017, 4, 20),
+      })
+      .execute();
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<any> {}
+}
