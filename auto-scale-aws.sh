0469eb37642787861630badca2472c7cb6266206
Philip Forsberg <philip.forsberg@etimo.se>
2020-06-25 14:02:44 +0200

    Change variable names and added echos.

--- auto-scale-aws.sh
+++ auto-scale-aws.sh
@@ -11,69 +11,79 @@ remove_double_quotes() {
 }
 
 off_season="Off season"
-volume_small="t2.small"
-volume_micro="t2.micro"
+instance_type_micro="t2.micro"
+instance_type_small="t2.small"
 instance_id="i-0ae22e94380789482"
 
 
 # Get instance volume
-instance_info=$(aws ec2 describe-instances \
---instance-ids  $instance_id \
---query "Reservations[*].Instances[*].{Status:State.Name,InstanceID:InstanceId,Instancetype:InstanceType}")
-instance_volume=$(jq '.[0][0].Instancetype' <<< $instance_info)
-instance_volume=$(remove_double_quotes "$instance_volume")
+instance_info=$(aws ec2 describe-instances --instance-ids  $instance_id --query "Reservations[*].Instances[*].{Status:State.Name,Instancetype:InstanceType}")
+instance_type=$(jq '.[0][0].Instancetype' <<< $instance_info)
+instance_type=$(remove_double_quotes "$instance_type")
+echo "InstanceType is $instance_type"
 
 
 # Fetch current_seanson. 
-response=$(curl http://localhost:8080/api/seasons/current) # TODO Change url
+response=$(curl http://diamonds.etimo.se/api/seasons/current)
 current_season=$(jq '.data.name'  <<< $response)
 current_season=$(remove_double_quotes "$CURRENT_SEASON")
 
 if [ "$current_season" = "$off_season" ]; then
-    volume=$volume_micro
+    new_instance_type=$instance_type_micro
 else
-    volume=$volume_small
+    new_instance_type=$instance_type_small
 fi
 
-# Exit if the volume is the same
-if [ "$instance_volume" = "$volume" ]; then 
-    echo "Volume is same, exiting cronjob"
+echo "InstanceType should be $new_instance_type"
+
+# Exit if the type is the same
+if [ "$instance_type" = "$new_instance_type" ]; then 
+    echo "Type is same, exiting cronjob"
     exit 1; 
 fi
 
 
-echo $instance_volume
-echo $volume
+echo "Current type: $instance_type"
+echo "New type: $new_instance_type"
+
+# Just testing - exiting now
+exit 1;
 exit 1;
 
-exit 1;
 
 # Stop instance
+echo "Stopping instance"
 aws ec2 stop-instances --instance-ids $instance_id
 
+
 ## Get instance info
-instance_info=$(aws ec2 describe-instances \
---instance-ids  $instance_id \
---query "Reservations[*].Instances[*].{Status:State.Name,InstanceID:InstanceId,Instancetype:InstanceType}")
+instance_info=$(aws ec2 describe-instances --instance-ids  $instance_id --query "Reservations[*].Instances[*].{Status:State.Name,Instancetype:InstanceType}")
 
 status=$(jq '.[0][0].Status' <<< $instance_info)
 status=$(remove_double_quotes "$status")
+
+
+echo "Instance status: $status"
+# Just testing - exiting now
+exit 1;
 
 #Is the instance still running?
 if [ "$status" = "running" ]; then 
     echo "Instance is still running"
     exit 1; 
-fi    
+fi
+
+echo "Instance is stopped!"
         
 # Change volume
-aws ec2 modify-instance-attribute \
---instance-id $instance_id \
---instance-type "{\"Value\": \"$volume\"}"
+aws ec2 modify-instance-attribute --instance-id $instance_id --instance-type "{\"Value\": \"$new_instance_type\"}"
+echo "Instance type changed to $new_instance_type"
 
 # Not checking if volume is correct because we need to start the instance again anyway!
 
 # START INSTANCE
 aws ec2 start-instances --instance-ids $instance_id
+echo "Instance started again"
 
 
 #Start docker containers! Need to try this!
