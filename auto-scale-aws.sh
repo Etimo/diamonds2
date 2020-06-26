62c6d5c30166c8ef33f73e7ef25c613ecaed83f9
Philip Forsberg <philip.forsberg@etimo.se>
2020-06-26 11:39:31 +0200

    Added sleep to wait for instance to be stopped.

--- auto-scale-aws.sh
+++ auto-scale-aws.sh
@@ -10,10 +10,18 @@ remove_double_quotes() {
     echo $(sed -e 's/^"//' -e 's/"$//' <<<"$1")
 }
 
+fetch_status() {
+    instance_info=$(aws ec2 describe-instances --instance-ids  $instance_id --query "Reservations[*].Instances[*].{Status:State.Name,Instancetype:InstanceType}")
+    status=$(jq '.[0][0].Status' <<< $instance_info)
+    status=$(remove_double_quotes "$status")
+}
+
+# We might need to load env variables (DIAMONDS_AWS_INSTANCE_ID)
+
 off_season="Off season"
 instance_type_micro="t2.micro"
 instance_type_small="t2.small"
-instance_id=$AWS_INSTANCE_ID
+instance_id=$DIAMONDS_AWS_INSTANCE_ID
 
 if [ -z "$instance_id" ]; then
     echo "Instance id not found"
@@ -40,38 +48,47 @@ else
 fi
 
 # Set to always micro - Testing
-new_instance_type=$instance_type_micro
+new_instance_type=$instance_type_small
 
 echo "InstanceType should be $new_instance_type"
+
 
 # Exit if the type is the same
 if [ "$instance_type" == "$new_instance_type" ]; then
     echo "Type is same, exiting cronjob"
-    exit 1; 
+    exit 1;
 fi
+
+
 
 
 echo "Current type: $instance_type"
 echo "New type: $new_instance_type"
 
+new_instance_type=$instance_type_micro
 
 # Stop instance
 echo "Stopping instance"
 aws ec2 stop-instances --instance-ids $instance_id
 
 
-## Get instance info
-instance_info=$(aws ec2 describe-instances --instance-ids  $instance_id --query "Reservations[*].Instances[*].{Status:State.Name,Instancetype:InstanceType}")
 
-status=$(jq '.[0][0].Status' <<< $instance_info)
-status=$(remove_double_quotes "$status")
+# Control that the instance is stopped!
+fetch_status
 
-#Is the instance still running?
-if [ "$status" == "running" ]; then
-    echo "Instance is still running"
-    exit 1; 
-fi
+#Waiting for the instance to be stopped. (total 100sec)
+sleeps=0
+while [ "$status" != "stopped" ]
+do
+    echo $status
+    sleep 20
+    sleeps=$((sleeps + 1))
+    fetch_status
+    if [ "$sleeps" == 5 ]; then exit 1; fi
+done
 
+
+echo "$status"
 echo "Instance is stopped!"
         
 # Change instance type
@@ -84,6 +101,3 @@ echo "Instance type changed to $new_instance_type"
 aws ec2 start-instances --instance-ids $instance_id
 echo "Instance started again"
 
-
-#Start docker containers! Need to try this!
-# https://stackoverflow.com/questions/58141265/how-to-auto-restart-a-docker-compose-cluster-when-ec2-instance-reboots
