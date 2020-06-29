400be4fea572bc70b9d0e6a873cec3fe6078d559
ForsbergPhilip <60732259+ForsbergPhilip@users.noreply.github.com>
2020-06-29 11:19:28 +0200

    Feat/add aws scaling (#128)
    * Added restart always to prod run docker-compose.
    
    * Added restart always to prod run docker-compose.
    
    * First edition of aws auto scale script.
    
    * Change variable names and added echos.
    
    * Change to lowercase.
    
    * Change volume to instance type.
    
    * Changed to double equal signs.
    
    * Changed scale script for testing.
    
    * Fetching instance_id from env variables and exiting if it does not exist.
    
    * Added sleep to wait for instance to be stopped.
    
    * Setting home directory for cronjob.

--- auto-scale-aws.sh
+++ auto-scale-aws.sh
@@ -10,23 +10,31 @@ remove_double_quotes() {
     echo $(sed -e 's/^"//' -e 's/"$//' <<<"$1")
 }
 
+# Fetching instance type and status
+fetch_instance_information() {
+    instance_info=$(aws ec2 describe-instances --instance-ids  $1 --query "Reservations[*].Instances[*].{Status:State.Name,Instancetype:InstanceType}")
+    status=$(jq '.[0][0].Status' <<< $instance_info)
+    status=$(remove_double_quotes "$status")
+    instance_type=$(jq '.[0][0].Instancetype' <<< $instance_info)
+    instance_type=$(remove_double_quotes "$instance_type")
+}
+
+# Setting home directory when running cronjob
+export HOME=/root
+
 off_season="Off season"
 instance_type_micro="t2.micro"
 instance_type_small="t2.small"
-instance_id=$AWS_INSTANCE_ID
+instance_id=$DIAMONDS_AWS_INSTANCE_ID
 
 if [ -z "$instance_id" ]; then
     echo "Instance id not found"
     exit 1
 fi
 
-
 # Get instance type
-instance_info=$(aws ec2 describe-instances --instance-ids  $instance_id --query "Reservations[*].Instances[*].{Status:State.Name,Instancetype:InstanceType}")
-instance_type=$(jq '.[0][0].Instancetype' <<< $instance_info)
-instance_type=$(remove_double_quotes "$instance_type")
+fetch_instance_information $instance_id
 echo "InstanceType is $instance_type"
-
 
 # Fetch current_seanson. 
 response=$(curl http://diamonds.etimo.se/api/seasons/current)
@@ -39,38 +47,41 @@ else
     new_instance_type=$instance_type_small
 fi
 
-# Set to always micro - Testing
-new_instance_type=$instance_type_micro
+# Set to always small - Testing
+new_instance_type=$instance_type_small
 
 echo "InstanceType should be $new_instance_type"
+
 
 # Exit if the type is the same
 if [ "$instance_type" == "$new_instance_type" ]; then
     echo "Type is same, exiting cronjob"
-    exit 1; 
+    exit 1;
 fi
-
 
 echo "Current type: $instance_type"
 echo "New type: $new_instance_type"
 
+# Set to always micro - Testing
+new_instance_type=$instance_type_micro
 
 # Stop instance
 echo "Stopping instance"
 aws ec2 stop-instances --instance-ids $instance_id
 
+# Control that the instance is stopped!
+fetch_instance_information $instance_id
 
-## Get instance info
-instance_info=$(aws ec2 describe-instances --instance-ids  $instance_id --query "Reservations[*].Instances[*].{Status:State.Name,Instancetype:InstanceType}")
-
-status=$(jq '.[0][0].Status' <<< $instance_info)
-status=$(remove_double_quotes "$status")
-
-#Is the instance still running?
-if [ "$status" == "running" ]; then
-    echo "Instance is still running"
-    exit 1; 
-fi
+#Waiting for the instance to be stopped. (total 100sec)
+sleeps=0
+while [ "$status" != "stopped" ]
+do
+    echo $status
+    sleep 20
+    sleeps=$((sleeps + 1))
+    fetch_instance_information $instance_id
+    if [ "$sleeps" == 5 ]; then exit 1; fi
+done
 
 echo "Instance is stopped!"
         
@@ -84,6 +95,3 @@ echo "Instance type changed to $new_instance_type"
 aws ec2 start-instances --instance-ids $instance_id
 echo "Instance started again"
 
-
-#Start docker containers! Need to try this!
-# https://stackoverflow.com/questions/58141265/how-to-auto-restart-a-docker-compose-cluster-when-ec2-instance-reboots
