6f987f4dd206979e5b4b86866e9910647e177207
Philip Forsberg <philip.forsberg@etimo.se>
2020-07-01 10:09:29 +0200

    Removed backup in migrations, created backup manually.

--- backend/src/migration/1593585305080-add-password.ts
+++ backend/src/migration/1593585305080-add-password.ts
@@ -1,23 +1,11 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 import { BotRegistrationsEntity } from "src/db/models/botRegistrations.entity";
-import fs from "fs";
 
 export class addPassword1593585305080 implements MigrationInterface {
   name = "addPassword1593585305080";
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     // Deleting all "old" bots. They have to register them again with a password.
-
-    // Create backup first
-    const bots = await queryRunner.manager
-      .createQueryBuilder(BotRegistrationsEntity, "bot_registrations")
-      .getMany();
-
-    fs.writeFile("mynewfile3.txt", bots, function(err) {
-      if (err) throw err;
-      console.log("Saved!");
-    });
-
     await queryRunner.manager
       .createQueryBuilder()
       .delete()
