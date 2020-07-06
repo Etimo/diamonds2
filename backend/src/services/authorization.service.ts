bc6af67e816c2f8e8e5a85361b8e8d1c157cc672
Philip Forsberg <philip.forsberg@etimo.se>
2020-07-06 16:05:27 +0200

    Created AuthorizationService to authorize requests.

---
+++ backend/src/services/authorization.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from "@nestjs/common";
+import { Request } from "express";
+import * as crypto from "crypto";
+import * as qs from "qs";
+import UnauthorizedError from "../errors/unauthorized.error";
+
+@Injectable()
+export class AuthorizationService {
+  async isSlackRequest(request: Request): Promise<void> {
+    // Validating that the request was send from slack.
+    const slackSigningSecret = process.env["SLACK_SIGNING_SECRET"];
+    const slackSignature = request.headers["x-slack-signature"];
+    const requestBody = qs.stringify(request.body, { format: "RFC1738" });
+    const timestamp = request.headers["x-slack-request-timestamp"].toString();
+
+    if (!timestamp || !slackSignature) {
+      this.throwUnauthorized();
+    }
+    const time = Math.floor(new Date().getTime() / 1000);
+
+    // Ignore request if its older than 5 min.
+    if (Math.abs(time - parseInt(timestamp)) > 300) {
+      this.throwUnauthorized();
+    }
+
+    // Create my signature with request data and slackSigningSecret
+    const sigBasestring = "v0:" + timestamp + ":" + requestBody;
+    const mySignature =
+      "v0=" +
+      crypto
+        .createHmac("sha256", slackSigningSecret)
+        .update(sigBasestring, "utf8")
+        .digest("hex");
+
+    // Compare my signature with x-slack-signature
+    if (
+      !crypto.timingSafeEqual(
+        Buffer.from(mySignature, "utf8"),
+        Buffer.from(slackSignature.toString(), "utf8"),
+      )
+    ) {
+      console.log("SIGNING ERROR");
+      this.throwUnauthorized();
+    }
+
+    console.log("SIGNING OK");
+  }
+
+  private throwUnauthorized() {
+    throw new UnauthorizedError("You are not allowed to call this endpoint");
+  }
+}
