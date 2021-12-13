d89c3bec323d5ec917ee259104ee9cf529ef70c7
Daniel Winther <daniel.kirgiopoulos@gmail.com>
2021-12-13 21:47:32 +0100

    fix

--- backend/src/services/high-scores.service.spec.ts
+++ backend/src/services/high-scores.service.spec.ts
@@ -31,86 +31,9 @@ describe("HighScoresService", () => {
     highscoresRepository = module.get<HighscoresRepository>(
       HighscoresRepository,
     );
+  });
+
+  afterEach(() => {
     jest.clearAllMocks();
   });
-
-  it("should be defined", () => {
-    expect(highScoresService).toBeDefined();
-    expect(seasonService).toBeDefined();
-  });
-
-  // it("Add new score", async () => {
-  //   let newHighScoreDto = {
-  //     botName: testBotName,
-  //     score: 44,
-  //     seasonId: "Off Season",
-  //   };
-
-  //   const mock = jest.spyOn(highscoresRepository, "create");
-
-  //   await highScoresService.addOrUpdate(newHighScoreDto);
-
-  //   expect(mock).toHaveBeenCalledTimes(1);
-  // });
-
-  // it("getBotScore", async () => {
-  //   let highscore = {
-  //     botName: testBotName,
-  //     score: 100,
-  //     seasonId: "Off Season",
-  //   };
-
-  //   const mock = jest
-  //     .spyOn(highscoresRepository, "getBotScore")
-  //     .mockReturnValue(new Promise(resolve => resolve([highscore])));
-
-  //   expect(await highScoresService.getBotScore(highscore)).toEqual([highscore]);
-  // });
-
-  // test("Update score", async () => {
-  //   let botLowScore: HighscoreDto = {
-  //     botName: testBotName,
-  //     score: 44,
-  //     seasonId: "Off Season",
-  //   };
-  //   let botHighScore = {
-  //     botName: testBotName,
-  //     score: 84,
-  //     seasonId: "Off Season",
-  //   };
-
-  //   const mock = jest.spyOn(highscoresRepository, "getBestBotScore");
-  //   const mockUpdate = jest.spyOn(highscoresRepository, "updateBestBotScore");
-
-  //   await highScoresService.addOrUpdate(botHighScore);
-
-  //   expect(mockUpdate).toHaveBeenCalledWith(botHighScore);
-  // });
-
-  // it("Ignore lower score", async () => {
-  //   let botHighScore = {
-  //     botName: testBotName,
-  //     score: 5,
-  //     seasonId: "Off Season",
-  //   };
-  //   let botLowScore = {
-  //     botName: testBotName,
-  //     score: 4,
-  //     seasonId: "Off Season",
-  //   };
-
-  //   await highScoresService.addOrUpdate(botHighScore);
-
-  //   await highScoresService.addOrUpdate(botLowScore);
-
-  //   // Save calls should not increase.
-  //   expect(repositoryMock.save).toHaveBeenCalledTimes(1);
-  //   expect(update).toHaveBeenCalledTimes(0);
-
-  //   let highscoreDtoItems = await highScoresService.getBotScore(botLowScore);
-
-  //   expect(highscoreDtoItems.length).toEqual(1);
-
-  //   expect(highscoreDtoItems[0].score).toEqual(botHighScore.score);
-  // });
 });
