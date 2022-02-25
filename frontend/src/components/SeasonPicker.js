809511abb926465af2db9c06fb5ef00eb48f97b9
Andre Hansson <andre.hansson@etimo.se>
2022-02-25 15:26:35 +0100

    Added RulesModal component

--- frontend/src/components/SeasonPicker.js
+++ frontend/src/components/SeasonPicker.js
@@ -2,18 +2,23 @@ import React from "react";
 import { useSeasons } from "../hooks";
 import Select from "../blocks/Select";
 
-const SeasonPicker = ({ onChange, value }) => {
+const SeasonPicker = ({ onChange, value, setRulesVisible }) => {
   const seasons = useSeasons();
   return (
     <Select>
       <Select.Title>Select Season</Select.Title>
-      <Select.Picker onChange={onChange} value={value}>
-        {seasons.map(season => (
-          <option key={season.id} value={season.id}>
-            {season.name}
-          </option>
-        ))}
-      </Select.Picker>
+      <Select.PickerWrapper>
+        <Select.Picker onChange={onChange} value={value}>
+          {seasons.map(season => (
+            <option key={season.id} value={season.id}>
+              {season.name}
+            </option>
+          ))}
+        </Select.Picker>
+        <Select.HoverText onClick={() => setRulesVisible(true)}>
+          Season rules
+        </Select.HoverText>
+      </Select.PickerWrapper>
     </Select>
   );
 };
