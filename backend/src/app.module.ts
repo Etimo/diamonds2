db8156e1f25eae008c95742dc8b75fa1f86af5c5
Daniel Winther <daniel.kirgiopoulos@gmail.com>
2023-02-16 15:08:54 +0100

    auto fix imports and format on save

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { AppService } from './app.service';
 import { BoardsController } from './controllers/boards.controller';
 import { BotsController } from './controllers/bots.controller';
 import { HighscoresController } from './controllers/highscores.controller';
@@ -9,15 +7,11 @@ import { SeasonsController } from './controllers/seasons.controller';
 import { SlackController } from './controllers/slack.controller';
 import { TeamsController } from './controllers/teams.controller';
 import { DatabaseModule } from './database.module';
-import { BoardConfigEntity } from './db/models/boardConfig.entity';
-import { BotRegistrationsEntity } from './db/models/botRegistrations.entity';
-import { HighScoreEntity } from './db/models/highScores.entity';
-import { RecordingsEntity } from './db/models/recordings.entity';
-import { SeasonsEntity } from './db/models/seasons.entity';
-import { TeamsEntity } from './db/models/teams.entity';
 import { HighscoresRepository } from './db/repositories/highscores.repository';
 import { RecordingsRepository } from './db/repositories/recordings.repository';
 import { CustomLogger } from './logger';
+import { repositoryProviders } from './repository.providers';
+import { AuthorizationService } from './services/authorization.service';
 import { BoardConfigService } from './services/board-config.service';
 import { BoardsService } from './services/board.service';
 import { BotsService } from './services/bots.service';
@@ -26,29 +20,6 @@ import { RecordingsService } from './services/recordings.service';
 import { SeasonsService } from './services/seasons.service';
 import { SlackService } from './services/slack.service';
 import { TeamsService } from './services/teams.service';
-import { repositoryProviders } from './repository.providers';
-import { AuthorizationService } from './services/authorization.service';
-
-const dbConfig: TypeOrmModuleOptions = {
-  type: 'postgres',
-  host: process.env['TYPEORM_HOST'],
-  port: parseInt(process.env['TYPEORM_PORT']),
-  username: process.env['TYPEORM_USERNAME'],
-  password: process.env['TYPEORM_PASSWORD'],
-  database: process.env['TYPEORM_DATABASE'],
-  synchronize: process.env['ENVIRONMENT'] === 'development',
-  entities: [
-    HighScoreEntity,
-    BotRegistrationsEntity,
-    SeasonsEntity,
-    TeamsEntity,
-    RecordingsEntity,
-    BoardConfigEntity,
-  ],
-  migrationsTableName: 'migration',
-  migrations: ['dist/migration/*.{ts,js}'],
-  ssl: false,
-};
 
 @Module({
   controllers: [
@@ -60,19 +31,7 @@ const dbConfig: TypeOrmModuleOptions = {
     SlackController,
     TeamsController,
   ],
-  imports: [
-    DatabaseModule,
-    // TypeOrmModule.forRoot(dbConfig),
-
-    // TypeOrmModule.forFeature([
-    //   HighScoreEntity,
-    //   BotRegistrationsEntity,
-    //   SeasonsEntity,
-    //   TeamsEntity,
-    //   RecordingsEntity,
-    //   BoardConfigEntity,
-    // ]),
-  ],
+  imports: [DatabaseModule],
   providers: [
     ...repositoryProviders,
     BoardsService,
