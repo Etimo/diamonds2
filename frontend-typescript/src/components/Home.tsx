eaa8bd677f9d3e1e81acee20c3704dcb6f126c3c
hejsaga <sagaswahn@hotmail.com>
2023-02-17 09:36:45 +0100

    feat: header (#267)

---
+++ frontend-typescript/src/components/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Grid } from './Grid';
+import { SideMenu } from './SideMenu';
+
+export const Home = () => {
+  return (
+    <div className="bg-white w-screen h-screen flex flex-col">
+      <div className="flex-1 grid grid-cols-[1fr_30%]">
+        <Grid />
+        <SideMenu />
+      </div>
+    </div>
+  );
+};
