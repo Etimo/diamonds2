966abda7cadfc9d7c9b6d717c8981bc20dc910d3
Malin Wadholm <malinwadholm@Etimo.local>
2023-09-14 10:33:26 +0200

    feat: add etimo-link in footer

--- frontend-typescript/src/components/Footer.tsx
+++ frontend-typescript/src/components/Footer.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import etimoLogo from '../assets/etimoLogo.png';
 
 export const Footer = () => {
@@ -5,7 +6,9 @@ export const Footer = () => {
     <div className="dark:bg-gray-800 pt-[3rem] lg:pt-[10rem] pb-8 w-full flex justify-center items-center">
       <h1>Powered by</h1>
       <div className="w-[100px] ml-5">
-        <img src={etimoLogo} alt="etimo-logo" />
+        <Link to="https://etimo.se/" target="_blank">
+          <img src={etimoLogo} alt="etimo-logo" />
+        </Link>
       </div>
     </div>
   );
