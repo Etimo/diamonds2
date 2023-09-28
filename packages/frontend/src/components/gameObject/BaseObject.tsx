2ab261d3e9a5231992c90f82d414c30815093d62
Malin <malin.wadholm@etimo.se>
2023-09-28 16:44:14 +0200

    chore: remove unnecessary comments

--- packages/frontend/src/components/gameObject/BaseObject.tsx
+++ packages/frontend/src/components/gameObject/BaseObject.tsx
@@ -3,14 +3,6 @@ import { FC, memo } from 'react';
 import { base } from '../images';
 import { CommonGameObject } from './CommonGameObject';
 
-// const getCharacterName = (gameObject: GameObject): string =>
-//   gameObject.properties?.name || '';
-
-// const getCharacterImg = (gameObject: GameObject): string => {
-//   const baseType = gameObject.type.includes('Bot') ? botBase : base;
-//   return baseType;
-// };
-
 export const BaseComponent: FC<BaseGameObjectProperties> = memo(({ name }) => {
   return <CommonGameObject characterName={name} characterImg={base} />;
 });
--- packages/frontend/src/components/gameObject/BotObject.tsx
+++ packages/frontend/src/components/gameObject/BotObject.tsx
@@ -3,14 +3,6 @@ import { FC, memo } from 'react';
 import { robot } from '../images';
 import { CommonGameObject } from './CommonGameObject';
 
-// const getCharacterName = (gameObject: GameObject): string =>
-//   gameObject.properties?.name || '';
-
-// const getCharacterImg = (gameObject: GameObject): string => {
-//   const botType = gameObject.type.includes('Diamond') ? botDiamond : robot;
-//   return botType;
-// };
-
 export const BotComponent: FC<BotGameObjectProperties> = memo(({ name }) => {
   return <CommonGameObject characterName={name} characterImg={robot} />;
 });
