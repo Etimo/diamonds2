e6f417e279568a6f3528418e2e11ab78f840ca97
Malin <malin.wadholm@etimo.se>
2023-10-02 11:26:38 +0200

    chore: add RuleItem for Rules component

--- packages/frontend/src/components/Rules.tsx
+++ packages/frontend/src/components/Rules.tsx
@@ -26,37 +26,44 @@ export const Rules: FC<RulesProps> = memo((props) => {
 
         {seasonRules ? (
           <>
-            <label className="text-label mb-0">Grid size</label>
-            <p className="mt-0 mb-2">{gridSize}</p>
-            <label className="text-label mb-0">Delay between moves</label>
-            <p className="mt-0 mb-2">{seasonRules.minimumDelayBetweenMoves}</p>
-            <label className="text-label mb-0">Round length</label>
-            <p className="mt-0 mb-2">{seasonRules.sessionLength}</p>
-            <label className="text-label mb-0">Inventory size</label>
-            <p className="mt-0 mb-2">{seasonRules.inventorySize}</p>
-            <label className="text-label mb-0">Tackling</label>
-            <p className="mt-0 mb-2">{setOnOff(seasonRules.canTackle)}</p>
-            <label className="text-label mb-0">Teleporters</label>
-            <p className="mt-0 mb-2">{setOnOff(seasonRules.teleporters)}</p>
-            <label className="text-label mb-0">Number of teleporters</label>
-            <p className="mt-0 mb-2">{seasonRules.teleporters}</p>
-            <label className="text-label mb-0">
-              Teleporter relocation time
-            </label>
-            <p className="mt-0 mb-2">{seasonRules.teleportRelocation}</p>
-            {seasonInfo ? (
-              <>
-                <label className="text-label mb-0">Season Ends</label>
-                <p className="mt-0 mb-2">
-                  {new Date(seasonInfo.endDate).toLocaleDateString('en-US', {
+            <RuleItem label="Grid size" value={gridSize} />
+            <RuleItem
+              label="Delay between moves"
+              value={seasonRules.minimumDelayBetweenMoves}
+            />
+            <RuleItem label="Round length" value={seasonRules.sessionLength} />
+            <RuleItem
+              label="Inventory size"
+              value={seasonRules.inventorySize}
+            />
+            <RuleItem
+              label="Tackling"
+              value={setOnOff(seasonRules.canTackle)}
+            />
+            <RuleItem
+              label="Teleporters"
+              value={setOnOff(seasonRules.teleporters)}
+            />
+            <RuleItem
+              label="Number of teleporters"
+              value={seasonRules.teleporters}
+            />
+            <RuleItem
+              label="Teleporter relocation time"
+              value={seasonRules.teleportRelocation}
+            />
+            {seasonInfo && (
+              <RuleItem
+                label="Season Ends"
+                value={new Date(seasonInfo.endDate).toLocaleDateString(
+                  'en-US',
+                  {
                     year: 'numeric',
                     month: 'long',
                     day: 'numeric',
-                  })}
-                </p>
-              </>
-            ) : (
-              ''
+                  },
+                )}
+              />
             )}
           </>
         ) : (
@@ -67,9 +74,19 @@ export const Rules: FC<RulesProps> = memo((props) => {
         x
       </button>
     </Modal>
-  ) : (
-    <></>
-  );
+  ) : null;
 });
 
 Rules.displayName = 'Rules';
+
+type RuleItemProps = {
+  label: string;
+  value: string | number;
+};
+
+const RuleItem: FC<RuleItemProps> = ({ label, value }) => (
+  <>
+    <label className="text-label mb-0">{label}</label>
+    <p className="mt-0 mb-2">{value}</p>
+  </>
+);
