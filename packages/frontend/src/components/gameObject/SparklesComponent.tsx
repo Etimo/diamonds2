30a881a619e9b684693e4bd44355518365a73fd4
Malin <malin.wadholm@etimo.se>
2023-10-02 09:39:33 +0200

    chore: adapt sparkles component for light mode, remove unused css

--- packages/frontend/src/components/gameObject/SparklesComponent.tsx
+++ packages/frontend/src/components/gameObject/SparklesComponent.tsx
@@ -9,7 +9,7 @@ export const SparklesComponent: FC = () => {
             <path
               id="large"
               d="M41.25,40 L42.5,10 L43.75,40 L45,41.25 L75,42.5 L45,43.75 L43.75,45 L42.5,75 L41.25,45 L40,43.75 L10,42.5 L40,41.25z"
-              fill="white"
+              fill="#badbff"
             ></path>
           </g>
           <g className="large-2" transform="rotate(45)">
